e927a9ea59dc2623a300a7ae30dfe2ba8bf02f01
Gilbert Veitia Urbay <gveitia13@gmail.com>
2023-05-01 02:58:28 -0400

    video 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HolaMundo';
+  nombre = 'Juan'
+  textPlaceholder = 'Escriba algo'
+  desabilitado = true
+  imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/640px-Angular_full_color_logo.svg.png'
+
+  constructor() {
+    setInterval(() => {
+      this.desabilitado = false
+      this.nombre = 'Javier'
+    }, 3000)
+  }
 }
